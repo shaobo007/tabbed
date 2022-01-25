dce6022a19e28f95f1f14d0975cefe587011bbfb
Pual Zhang <zsb940680883@outlook.com>
2022-01-25 11:51:45 +0800

    color changed

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "monospace:size=9";
-static const char* normbgcolor  = "#222222";
-static const char* normfgcolor  = "#cccccc";
-static const char* selbgcolor   = "#555555";
-static const char* selfgcolor   = "#ffffff";
+static const char font[]        = "Hack Nerd Font Mono:size=12";
+static const char* normbgcolor  = "#44475a";
+static const char* normfgcolor  = "#f8f8f2";
+static const char* selbgcolor   = "#bd93f9";
+static const char* selfgcolor   = "#f8f8f2";
 static const char* urgbgcolor   = "#111111";
 static const char* urgfgcolor   = "#cc0000";
 static const char before[]      = "<";
