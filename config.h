28b23368d3dfa9214f535bf8d8e69090d991ce04
Pual Zhang <zsb940680883@outlook.com>
2022-01-27 10:22:58 +0800

    mk file mod

--- config.h
+++ config.h
@@ -36,13 +36,13 @@ static Bool npisrelative  = False;
 #define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key              function     argument */
-	{ MODKEY|Mod1Mask,     XK_Return,       focusonce,   { 0 } },
-	{ MODKEY|Mod1Mask,     XK_Return,       spawn,       { 0 } },
+	{ MODKEY|Mod1Mask,      XK_Return,       focusonce,   { 0 } },
+	{ MODKEY|Mod1Mask,      XK_Return,       spawn,       { 0 } },
 
-	{ MODKEY|Mod1Mask,     XK_l,            rotate,      { .i = +1 } },
-	{ MODKEY|Mod1Mask,     XK_h,            rotate,      { .i = -1 } },
-	{ MODKEY|Mod1Mask,     XK_j,            movetab,     { .i = -1 } },
-	{ MODKEY|Mod1Mask,     XK_k,            movetab,     { .i = +1 } },
+	{ MODKEY|Mod1Mask,      XK_l,            rotate,      { .i = +1 } },
+	{ MODKEY|Mod1Mask,      XK_h,            rotate,      { .i = -1 } },
+	{ MODKEY|Mod1Mask,      XK_j,            movetab,     { .i = -1 } },
+	{ MODKEY|Mod1Mask,      XK_k,            movetab,     { .i = +1 } },
 	{ MODKEY,               XK_Tab,          rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,        spawn,       SETPROP("_TABBED_SELECT_TAB") },
@@ -58,9 +58,8 @@ static Key keys[] = {
 	{ MODKEY,               XK_0,            move,        { .i = 9 } },
 
 	{ MODKEY,               XK_q,            killclient,  { 0 } },
-
 	{ MODKEY,               XK_u,            focusurgent, { 0 } },
-	{ MODKEY|Mod1Mask,     XK_u,            toggle,      { .v = (void*) &urgentswitch } },
+	{ MODKEY|Mod1Mask,      XK_u,            toggle,      { .v = (void*) &urgentswitch } },
 
 	{ 0,                    XK_F11,          fullscreen,  { 0 } },
 
